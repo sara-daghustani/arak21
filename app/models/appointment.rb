3747987ad604fd4d0ab487ea3ddd9a799d24aad8
Abdullah <a_t1411@hotmail.com>
2019-09-04 11:55:15 +0300

    Dynamic and relation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,10 @@
 class Appointment
   include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
   field :date, type: Date
   field :time, type: Time
   field :doctor_id, type: Integer
   field :patient_id, type: Integer
+  belongs_to :doctor
+  belongs_to :patient
 end
