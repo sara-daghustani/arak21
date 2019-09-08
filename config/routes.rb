cd04128bb677028e83fc653c9df8134ea975aba8
Abdullah <a_t1411@hotmail.com>
2019-09-08 15:31:27 +0300

    add doctor appointment page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/home", to: "pages#home"
   get "/aboutus" => "pages#aboutus"
   get "/profile" => "pages#profile"
+  get "/doctorAppointments" => "pages#doctorAppointments"
   resources :doctors
   resources :appointments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
