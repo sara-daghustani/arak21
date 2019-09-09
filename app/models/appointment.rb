b6761d50964bcbe92c21ca0a7ca07d852a0fac00
Abdullah <a_t1411@hotmail.com>
2019-09-09 17:03:40 +0300

    fix the datetime

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
-  field :appointment_on, type: DateTime
+  field :appointment_on, type: DateTime, default: { 'CURRENT_TIMESTAMP' }
   field :doctor_id, type: String
   field :user_id, type: String
   belongs_to :doctor
