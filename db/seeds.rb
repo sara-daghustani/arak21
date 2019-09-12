46c925453ddf1a4942a5b89c8920eda0fe6c4b37
Abdullah <a_t1411@hotmail.com>
2019-09-12 08:52:11 +0300

    trying the local

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-User.create(
-  email: "drahmed90@hotmail.com",
-  password: "123456",
-  password_confirmation: "123456",
-  first_name: "احمد",
-  last_name: "الغامدي",
-  phone: "1234567890",
-  admin: true
-)
